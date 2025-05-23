76e00e47601098142b6f5da438472a037bbca1e6
Jared Rigby <talk@jaredrigby.co.uk>
2025-05-23 09:51:49 +0100

    Added fullscreen settings

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ class GameState(Enum):
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
         pygame.display.set_caption('AI Plane Game')
         self.clock = pygame.time.Clock()
         self.active = True 
